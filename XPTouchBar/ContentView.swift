68d89696d3a81f0f2a92d9161fdb5f06c13c8268
Chris Kilding <590569+chriskilding@users.noreply.github.com>
2022-05-19 20:05:10 +0100

    Initial Commit

---
+++ XPTouchBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XPTouchBar
+//
+//  Created by Chris Kilding on 19/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XPTouchBar/XPTouchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  XPTouchBarApp.swift
+//  XPTouchBar
+//
+//  Created by Chris Kilding on 19/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct XPTouchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
